23274686aea28bd5ae9a6bd33ab7c60b6e4b4f63
Petr Chalupa <chalupapetr77@gmail.com>
2025-05-10 13:41:38 +0200

    Level editor level select

--- ui/src/main/java/ui/LevelEditor.java
+++ ui/src/main/java/ui/LevelEditor.java
@@ -4,10 +4,12 @@ import engine.Engine;
 import engine.core.LevelData;
 import engine.utils.LevelLoader;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,9 +21,11 @@ public class LevelEditor {
 	@FXML
 	private VBox selector;
 	@FXML
+	private TextField filterInput;
+	@FXML
 	private VBox levelSelect;
 	@FXML
-	private BorderPane editor;
+	private TabPane editor;
 
 	@FXML
 	private void clickMainMenu() throws IOException {
@@ -30,26 +34,41 @@ public class LevelEditor {
 	}
 
 	@FXML
+	private void clickCreateNew() {
+		System.out.println("create new level");
+		openEditor();
+	}
+
+	@FXML
+	private void clickImport() {
+		System.out.println("import level");
+		openEditor();
+	}
+
+	@FXML
+	private void filterSelect() {
+		loadLevelSelect(filterInput.getText().trim().toLowerCase());
+	}
+
+	@FXML
+	public void initialize() {
+		loadLevelSelect("");
+	}
+
 	private void openEditor() {
 		selector.setVisible(false);
 		editor.setVisible(true);
 	}
 
-	@FXML
-	public void initialize() {
-		loadLevelSelect();
-	}
-
-	private void loadLevelSelect() {
+	private void loadLevelSelect(String filter) {
 		List<LevelData> levels = Engine.getInstance().getLevelLoader().getImportedLevels();
 		levels.sort((level1, level2) -> level1.getName().compareToIgnoreCase(level2.getName()));
+		levels.removeIf(level -> !level.getName().toLowerCase().contains(filter));
 
 		levelSelect.getChildren().clear();
 		for (LevelData level : levels) {
-			// render todo
-			Label label = new Label(level.getName());
-			label.getStyleClass().add("no_levels_label");
-			levelSelect.getChildren().add(label);
+			HBox levelBox = renderLevel(level);
+			levelSelect.getChildren().add(levelBox);
 		}
 
 		if (levels.isEmpty()) {
@@ -59,11 +78,41 @@ public class LevelEditor {
 		}
 	}
 
-	public void loadLevel(String path) {
+	private HBox renderLevel(LevelData level) {
+		HBox levelBox = new HBox();
+		levelBox.getStyleClass().add("level_box");
+
+		ImageView thumbnailView = new ImageView();
+		thumbnailView.getStyleClass().add("thumbnail");
+		thumbnailView.setImage(Engine.getInstance().getResourceManager().getImg(level.getThumbnailPath()));
+
+		HBox infoBox = new HBox();
+		infoBox.getStyleClass().add("info_box");
+		HBox.setHgrow(infoBox, Priority.ALWAYS);
+
+		Label nameLabel = new Label(level.getName());
+		nameLabel.getStyleClass().add("level_name");
+
+		Region region = new Region();
+		HBox.setHgrow(region, Priority.ALWAYS);
+
+		Button loadButton = new Button("Load");
+		loadButton.getStyleClass().add("load_button");
+		loadButton.setOnAction(event -> loadLevel(level.getName()));
+
+		infoBox.getChildren().addAll(nameLabel, region, loadButton);
+		levelBox.getChildren().addAll(thumbnailView, infoBox);
+
+		return levelBox;
+	}
+
+	public void loadLevel(String name) {
+		openEditor();
+
 		Engine engine = App.getEngine();
 		engine.init(canvasParent, 10);
 
 		LevelLoader levelLoader = LevelLoader.getInstance();
-		levelLoader.loadLevel(path);
+		levelLoader.loadLevel(name);
 	}
 }
